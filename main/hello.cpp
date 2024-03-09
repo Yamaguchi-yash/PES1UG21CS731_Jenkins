1f39e8abf8bb1b07888cb7a87e253ee3e447ca6f
Yamaguchi-yash <150885876+Yamaguchi-yash@users.noreply.github.com>
2024-03-09 22:49:53 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Yashas the great\n";
+  cout << "Hello, Yashas PES1UG21CS731\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
